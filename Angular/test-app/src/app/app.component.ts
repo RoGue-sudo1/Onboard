527eccae6f8cbc1b6d22688e22aeeb6013bc4996
Harsh Rathore <harsh.rathore@fretron.com>
2024-02-20 15:13:58 +0530

    Practise on angular testing the all features

---
+++ Angular/test-app/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { ChildComponent } from './child/child.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, UserComponent, ChildComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'test-app';
+  isView = true;
+  users = [
+    { id: 0, name: 'Sarah' },
+    { id: 1, name: 'Amy' },
+    { id: 2, name: 'Rachel' },
+    { id: 3, name: 'Jessica' },
+    { id: 4, name: 'Poornima' },
+  ];
+
+  isEditable = true;
+
+  message = '';
+
+  greet() {
+    window.alert('Hello, there');
+  }
+
+  onMouseOver() {
+    this.message = 'way to go';
+  }
+  count = 0;
+  item = '';
+  incrementCount($event: number) {
+    this.count = $event;
+  }
+
+  addItem($event: string) {
+    this.item = $event;
+  }
+}
---
+++ Angular/test-app/src/app/user/user.component.ts
@@ -0,0 +1,14 @@
+import { Component,Input,Output,EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+     @Input() occupation = ''
+     @Input() name = ''
+    
+}
