6f538a932954e6557b6f815118c6c9d2f18ec7f5
Harsh Rathore <harsh.rathore@fretron.com>
2024-03-14 16:04:06 +0530

    completed transformation operators

--- Angular/Rxjs/src/app/transformation-operators/transformation-operators.component.ts
+++ Angular/Rxjs/src/app/transformation-operators/transformation-operators.component.ts
@@ -1,5 +1,20 @@
 import { Component } from '@angular/core';
-import { buffer, concatMap, concatMapTo, delay, exhaustMap, from, fromEvent, interval, map, mapTo, of, take } from 'rxjs';
+import {
+  buffer,
+  concatMap,
+  concatMapTo,
+  delay,
+  exhaustMap,
+  from,
+  fromEvent,
+  interval,
+  map,
+  mapTo,
+  mergeMap,
+  of,
+  switchMap,
+  take,
+} from 'rxjs';
 
 @Component({
   selector: 'app-transformation-operators',
@@ -14,62 +29,66 @@ export class TransformationOperatorsComponent {
 
     // 1. buffer() (Collects values from the past as an array, and emits that array only when another observable emits.)
 
-              const myInterval = interval(1000);
-              const bufferby = fromEvent(document, 'click');
+    const myInterval = interval(1000);
+    const bufferby = fromEvent(document, 'click');
 
-              const bufferExample = myInterval.pipe(buffer(bufferby));
+    const bufferExample = myInterval.pipe(buffer(bufferby));
 
-              const bufferSubscribe = bufferExample.subscribe((val) =>
-                console.log('Buffer:', val)
-              );
+    const bufferSubscribe = bufferExample.subscribe((val) =>
+      console.log('Buffer:', val)
+    );
 
     // 2. concatMap() (Projects each source value to an Observable which is merged in the output Observable, in a serialized fashion waiting for each one to complete before merging the next.)
 
-            const concatMapSource = of('Hello', 'Goodbye');
+    const concatMapSource = of('Hello', 'Goodbye');
 
-            const examplePromise = (val: any) =>
-              new Promise((resolve) => resolve(val + ' World!'));
+    const examplePromise = (val: any) =>
+      new Promise((resolve) => resolve(val + ' World!'));
 
-            const concatMapExample = concatMapSource.pipe(
-              concatMap((val) => examplePromise(val))
-            );
+    const concatMapExample = concatMapSource.pipe(
+      concatMap((val) => examplePromise(val))
+    );
 
-          
+    const concatMpaSubscribe = concatMapExample.subscribe((val) =>
+      console.log('ConcatMap:', val)
+    );
 
-            const concatMpaSubscribe = concatMapExample.subscribe((val) =>
-              console.log('ConcatMap:', val)
-            );
-
-    
     // 3. concatMapTo() (Projects each source value to the same Observable which is merged multiple times in the output Observable.)
 
-            const sampleInterval = interval(2000).pipe(take(5))
+    const sampleInterval = interval(2000).pipe(take(5));
 
-            const fakeRequest =of('Network request complete').pipe(delay(3000))
-            
-            console.log(new Date())
-            const concatMapToExample = sampleInterval.pipe(concatMapTo(fakeRequest))
-             
-            const concatMapToSubscribe = concatMapToExample.subscribe((val) =>
-              console.log('ConcatMapTo:'+ val + '\n '+ new Date())
-            );
+    const fakeRequest = of('Network request complete').pipe(delay(3000));
 
-            const interval$ = interval(1000)
+    console.log(new Date());
+    const concatMapToExample = sampleInterval.pipe(concatMapTo(fakeRequest));
 
-            const source = interval(2000).pipe(take(5))
+    const concatMapToSubscribe = concatMapToExample.subscribe((val) =>
+      console.log('ConcatMapTo:' + val + '\n ' + new Date())
+    );
 
-            /*
+    const interval$ = interval(1000);
+
+    const source = interval(2000).pipe(take(5));
+
+    /*
              ***Be Careful***: In situations like this where the source emits at a faster pace
                 than the inner observable completes, memory issues can arise.
                 (interval emits every 1 second, basicTimer completes every 5)
               */
-            // basicTimer will complete after 5 seconds, emitting 0,1,2,3,4
-            console.log(new Date())
-            const example = interval$.pipe(concatMapTo(source,(firstValue,secondValue)=> `${firstValue} ${secondValue}`))
+    // basicTimer will complete after 5 seconds, emitting 0,1,2,3,4
+    console.log(new Date());
+    const example = interval$.pipe(
+      concatMapTo(
+        source,
+        (firstValue, secondValue) => `${firstValue} ${secondValue}`
+      )
+    );
 
-            const subscribe = example.subscribe(val=>console.log('ConcatMapTo:'+ val + ' \n'+ new Date()))
+    const subscribe = example.subscribe((val) =>
+      console.log('ConcatMapTo:' + val + ' \n' + new Date())
+    );
 
-                /*
+    /*
                   output: 0 0
                           0 1
                           0 2
@@ -81,26 +100,28 @@ export class TransformationOperatorsComponent {
 
                 */
 
-    // 4. exhaustMap() 
-          /* Imagine you're at a coffee shop where each customer is 
+    // 4. exhaustMap()
+    /* Imagine you're at a coffee shop where each customer is 
             allowed to place only one order at a time and must wait 
             until that order is fully prepared before making another.
             If they try to order again while their coffee is still brewing,
-            the barista simply ignores them. That's the essence of exhaustMap.*/    
-            
-          const firstInterval =interval(1000).pipe(take(10))
-          const secondInterval = interval(1000).pipe(take(2))
+            the barista simply ignores them. That's the essence of exhaustMap.*/
 
-          const exhaustMapExample = firstInterval.pipe(exhaustMap((val) => {
-            console.log('Emmision in first interval:', val)
-            return secondInterval
-          }))
+    const firstInterval = interval(1000).pipe(take(10));
+    const secondInterval = interval(1000).pipe(take(2));
 
-          const exhaustMapSubscribe = exhaustMapExample.subscribe((val) =>
-            console.log('ExhaustMap:', val)
-          );
+    const exhaustMapExample = firstInterval.pipe(
+      exhaustMap((val) => {
+        console.log('Emmision in first interval:', val);
+        return secondInterval;
+      })
+    );
 
-            /*
+    const exhaustMapSubscribe = exhaustMapExample.subscribe((val) =>
+      console.log('ExhaustMap:', val)
+    );
+
+    /*
                 When we subscribed to the first interval, it starts to emit a values (starting 0).
                 This value is mapped to the second interval which then begins to emit (starting 0).  
                 While the second interval is active, values from the first interval are ignored.
@@ -120,30 +141,58 @@ export class TransformationOperatorsComponent {
                   0
                   1
               */
-      
+
     // 5. map() (Apply projection with each value from source.)
 
-          const mapSource = from([1,2,3,4,5])
+    const mapSource = from([1, 2, 3, 4, 5]);
 
-          /*
+    /*
              * now when we are using from operator, it will emit values/observables one by one
                so output will be like this: 11,12,13,14,15
 
                but when we are using of operator, i.e const mapSource = of([1,2,3,4,5])
                It will emit all the values at once, so output will be like this: 1,2,3,4,510
           */
-          
-          const mapExample = mapSource.pipe(map((val:any)=>val + 10))
-          const mapSubscribe = mapExample.subscribe((val) =>  console.log('Map:', val))
+
+    const mapExample = mapSource.pipe(map((val: any) => val + 10));
+    const mapSubscribe = mapExample.subscribe((val) =>
+      console.log('Map:', val)
+    );
 
     // 6. mapTo() (Map emissions to constant value.)
-        
-           const mapToSource = interval(2000).pipe(take(5))
 
-           const mapToExample = mapToSource.pipe(mapTo('Hello World!')).subscribe((val) => console.log('MapTo:', val))
-           
+    const mapToSource = interval(2000).pipe(take(5));
+
+    const mapToExample = mapToSource
+      .pipe(mapTo('Hello World!'))
+      .subscribe((val) => console.log('MapTo:', val));
 
 
+  // 7. mergeMap() 
+            /*
+                * Projects each source value to an Observable which is merged in the output Observable.\
+                * It is a combination of map and mergeAll.
+                * It is used when we want to merge the output of the inner observable with the outer observable.
+                * It is used when we want to handle asyncronous behavious 
+                * map() is only for handling synchronous behaviour.
+            */
 
+          const mergeMapPromise = (val:any)=> new Promise((resolve) => resolve(val + ' World from promise!'));
+
+          const mergeMapSource = of('Hello')
+
+          const mergeMapExample = mergeMapSource.pipe(mergeMap((val)=>mergeMapPromise(val)))
+
+          const mergeMapSubscribe = mergeMapExample.subscribe( val => console.log('MergeMap:', val))
+
+    // 8. switchMap() (Projects each source value to an Observable which is merged in the output Observable, emitting values only from the most recently projected Observable.)
+          
+          const switchMapSource = fromEvent(document,'click')
+          const switchMapExample = switchMapSource.pipe(switchMap(()=>interval(1000)))
+
+          const switchMapSubscribe = switchMapExample.subscribe(val => console.log('SwitchMap:', val))  
   }
+
+
+
 }
