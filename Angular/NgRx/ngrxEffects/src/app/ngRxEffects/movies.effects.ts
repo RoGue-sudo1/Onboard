9177e6304acd20f563809812710cda68eca30ceb
Harsh Rathore <harsh.rathore@fretron.com>
2024-03-21 12:08:47 +0530

    nochanges

--- Angular/NgRx/ngrxEffects/src/app/ngRxEffects/movies.effects.ts
+++ Angular/NgRx/ngrxEffects/src/app/ngRxEffects/movies.effects.ts
@@ -1,24 +1,27 @@
-import { Injectable } from "@angular/core";
-import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { EMPTY, catchError, exhaustMap, map } from "rxjs";
-import { MoviesService } from "../movies.service";
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { EMPTY, catchError, exhaustMap, map } from 'rxjs';
+import { MoviesService } from '../movies.service';
 
 @Injectable()
-
 export class MoviesEffects {
-
-    loadMovies$ = createEffect(() => this.actions$.pipe(
+  loadMovies$ = createEffect(() =>
+    this.actions$.pipe(
       ofType('[Movies Page] Load Movies'),
-      exhaustMap(() => this.moviesService.getMovies()
-        .pipe(
-          map(movies => ({ type: '[Movies API] Movies Loaded Success', payload: movies })),
+      exhaustMap(() =>
+        this.moviesService.getMovies().pipe(
+          map((movies) => ({
+            type: '[Movies API] Movies Loaded Success',
+            payload: movies,
+          })),
           catchError(() => EMPTY)
-        ))
+        )
       )
-    );
-  
-    constructor(
-      private actions$: Actions,
-      private moviesService: MoviesService
-    ) {}
-  }
+    )
+  );
+
+  constructor(
+    private actions$: Actions,
+    private moviesService: MoviesService
+  ) {}
+}
